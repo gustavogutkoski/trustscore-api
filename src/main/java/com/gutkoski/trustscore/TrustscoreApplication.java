26f7ac60e5fed953e34e733d2a992645abcbc072
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2025-01-13 14:52:02 -0300

    Initial commit

---
+++ src/main/java/com/gutkoski/trustscore/TrustscoreApplication.java
@@ -0,0 +1,13 @@
+package com.gutkoski.trustscore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustscoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustscoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gutkoski/trustscore/TrustscoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gutkoski.trustscore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrustscoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
