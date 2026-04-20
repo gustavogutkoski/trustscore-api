0d5929ae6a2e4b12f1fe09dbc0e02423222f94ab
Gustavo A. Gutkoski <gustavogutkoski2609@gmail.com>
2026-04-20 11:00:04 -0300

    chore: fix formatter

--- src/main/java/com/gutkoski/trustscore/config/SecurityConfig.java
+++ src/main/java/com/gutkoski/trustscore/config/SecurityConfig.java
@@ -8,17 +8,17 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class SecurityConfig {
 
-    //FIXME authenticate prometheus metrics
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/actuator/prometheus").permitAll()
-                        .anyRequest().authenticated()
-                );
+  // FIXME authenticate prometheus metrics
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    http.csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(
+            auth ->
+                auth.requestMatchers("/actuator/prometheus")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated());
 
-        return http.build();
-    }
+    return http.build();
+  }
 }
-
